858cf760da9dcafdc3a945b803b086c9d4d51894
an1l4 <1995anila@gmail.com>
2022-06-16 10:32:45 +0530

    GoPackages

---
+++ goPackages.go/exportedNames.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.Pi)  //Pi is exported form math package so should be capital
+}
---
+++ goPackages.go/mathPackage.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+import "math/rand"
+
+func main() {
+	fmt.Println("My favorite number is :",rand.Intn(1000))
+}
