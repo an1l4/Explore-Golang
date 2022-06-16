14c00fc0808140ad04e03620d300572c92234a7a
an1l4 <1995anila@gmail.com>
2022-06-16 10:15:28 +0530

    GoFormattingVerbs.go

---
+++ goFormattingVerbs.go/formattingForPrintf.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i = 20.5
+	var text = "Anila"
+
+	fmt.Printf("%v \n",i)
+	fmt.Printf("%#v \n",i)
+	fmt.Printf("%T \n",i)
+	fmt.Printf("%v%% \n",i)
+
+	fmt.Printf("%v \n",text)
+	fmt.Printf("%#v \n",text)
+	fmt.Printf("%T \n",text)
+	fmt.Printf("%v%% \n",text)
+
+
+}
