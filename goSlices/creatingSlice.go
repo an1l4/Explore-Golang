503f22dec3304c9378005125bbeb4e21a866958d
an1l4 <1995anila@gmail.com>
2022-06-21 13:04:16 +0530

    added 1 extra line to see the type of mySlice2

--- goSlices/creatingSlice.go
+++ goSlices/creatingSlice.go
@@ -8,8 +8,10 @@ func main() {
 	fmt.Println(cap(mySlice1))
 	fmt.Println(mySlice1)
 
-	mySlice2 := []string{"Go","Slices","are","Powerful"}
+	mySlice2 := []string{"Go", "Slices", "are", "Powerful"}
 	fmt.Println(len(mySlice2))
 	fmt.Println(cap(mySlice2))
 	fmt.Println(mySlice2)
+	//want to see the type of mySlice2
+	fmt.Printf("Type of the mySlice2 is %T \n", mySlice2) //type is []string (that means type is slice)
 }
