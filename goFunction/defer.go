36915c3343781e799254fbd62ce540a8ed7a3817
an1l4 <1995anila@gmail.com>
2022-06-22 07:58:36 +0530

    added-more-functionalities-with-defer

--- goFunction/defer.go
+++ goFunction/defer.go
@@ -4,6 +4,17 @@ import "fmt"
 
 func main() {
 	defer fmt.Println("World")
+	defer fmt.Println("one")
+	defer fmt.Println("two")
+	defer fmt.Println("three") //LIFO last in first out
 
 	fmt.Println("Hello")
+	myDefer()
+
+}
+
+func myDefer() {
+	for i := 0; i < 5; i++ {
+		defer fmt.Println(i)
+	}
 }
